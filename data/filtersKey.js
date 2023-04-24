8d79575c5ec7db4a508f2adb153b99ddd6cd3232
dejmedus <email96123@gmail.com>
2023-04-24 17:47:55 -0600

    update filters key

--- data/filtersKey.js
+++ data/filtersKey.js
@@ -1,3 +1,10 @@
 export const filtersKey = {
-    'favorites': ['Roboto', 'Lato', 'Inter']
+    'favorites': ['Roboto', 'Lato', 'Inter'],
+    'magic': ['Amarante', 'Berkshire Swash'],
+    'fancy': ['Almendra', 'Almendra Display', 'Astloch'],
+    'fun': ['Barrio'],
+    'childlike': ['Autour One', 'Balsamiq Sans', 'Gamja Flower'],
+    'handwriting': ['Bad Script', 'Beth Ellen', 'Dawning of a New Day'],
+    'less-known': ['Averia Gruesa Libre'],
+    'space': ['Bungee Hairline']
 }
