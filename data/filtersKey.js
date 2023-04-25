57dae21868a4215f5c88fa4a2e68cdfffe2f1755
dejmedus <email96123@gmail.com>
2023-04-25 10:50:08 -0600

    sort themes alpha

--- data/filtersKey.js
+++ data/filtersKey.js
@@ -1,10 +1,10 @@
 export const filtersKey = {
-    'fancy': ['Almendra', 'Almendra Display', 'Astloch', 'Bokor', 'IM Fell English SC', 'Grenze Gotisch', 'UnifrakturMaguntia', 'Pirata One'],
-    'fun': ['Barrio', 'Averia Gruesa Libre', 'Rampart One', 'Nabla', 'BIZ UDMincho', 'Amarante', 'Berkshire Swash'],
-    'childlike': ['Balsamiq Sans', 'Gamja Flower', 'Bonbon', 'Capriola', 'Dongle', 'Slackey'],
-    'handwriting': ['Bad Script', 'Beth Ellen', 'Dawning of a New Day', 'La Belle Aurore'],
-    'space': ['Bungee Hairline', 'Bruno Ace', 'Audiowide', 'Bai Jamjuree', 'Share Tech Mono', 'Michroma', 'Oxanium', 'Goldman'],
-    'modern': ['DM Serif Text', 'Yatra One', 'Varela', 'Inter Tight', 'Krub', 'Allerta', 'Darker Grotesque', 'Krona One', 'Alexandria'],
     'bold': ['Bebas Neue', 'Londrina Solid', 'Koulen', 'Bowlby One SC', 'Candal', 'Knewave'],
     'code': ['IBM Plex Mono', 'Space Mono', 'Anonymous Pro', 'JetBrains Mono', 'Overpass Mono', 'M PLUS 1'],
+    'cute': ['Balsamiq Sans', 'Gamja Flower', 'Bonbon', 'Capriola', 'Dongle', 'Slackey'],
+    'fancy': ['Almendra', 'Almendra Display', 'Astloch', 'Bokor', 'IM Fell English SC', 'Grenze Gotisch', 'UnifrakturMaguntia', 'Pirata One'],
+    'fun': ['Barrio', 'Averia Gruesa Libre', 'Rampart One', 'Nabla', 'BIZ UDMincho', 'Amarante', 'Berkshire Swash'],
+    'handwriting': ['Bad Script', 'Beth Ellen', 'Dawning of a New Day', 'La Belle Aurore'],
+    'modern': ['DM Serif Text', 'Yatra One', 'Varela', 'Inter Tight', 'Krub', 'Allerta', 'Darker Grotesque', 'Krona One', 'Alexandria'],
+    'space': ['Bungee Hairline', 'Bruno Ace', 'Audiowide', 'Bai Jamjuree', 'Share Tech Mono', 'Michroma', 'Oxanium', 'Goldman'],
 }
