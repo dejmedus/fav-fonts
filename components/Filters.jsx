d9ec7265669cf9c8eeb2151ebbdf9aecaabd2f80
dejmedus <email96123@gmail.com>
2023-04-25 13:42:51 -0600

    assign input css to var for readability

--- components/Filters.jsx
+++ components/Filters.jsx
@@ -4,23 +4,25 @@ import { ThemeContext } from '@/context/ThemeContext'
 import { filtersKey } from "@/data/filtersKey";
 import { SunSVG, MoonSVG, ResetSVG } from '@/assets/svg/icons';
 
-// dark/light mode, grid/list mode, and reset
 export default function Filters({ setOptions, options }) {
     const { theme, toggleTheme } = useContext(ThemeContext);
 
-    return (<div className={"flex items-center text-black dark:text-white justify-center gap-2 mb-2 dark:border-white border-black"}>
+    const inputCSS = "bg-white border-b-2 dark:bg-black text-inherit dark:border-gray-700 focus:border-black dark:focus:border-white focus:outline-none"
 
+    return (<div className="flex flex-wrap items-center justify-center w-full mb-2 border-black gap-x-2 dark:border-white">
         <label htmlFor="search">Search:</label>
-        <input className="bg-white border-b-2 dark:bg-black text-inherit dark:border-gray-700 focus:border-black dark:focus:border-white focus:outline-none" id="sampleText" type="text" placeholder='Search...' value={options.search} onChange={(e) => setOptions.search(e.target.value)} />
+        <input className={inputCSS} id="sampleText" type="text" placeholder='Search...' value={options.search} onChange={(e) => setOptions.search(e.target.value)} />
 
         <label htmlFor="sampleText">Sample Text:</label>
-        <input className="bg-white border-b-2 dark:bg-black text-inherit dark:border-gray-700 focus:border-black dark:focus:border-white focus:outline-none" id="sampleText" type="text" placeholder='Spinx of black quartz' value={options.sampleText} onChange={(e) => setOptions.sampleText(e.target.value)} />
+        <input className={inputCSS} id="sampleText" type="text" placeholder='Spinx of black quartz' value={options.sampleText} onChange={(e) => setOptions.sampleText(e.target.value)} />
 
         <label htmlFor="fontSize">Font Size:</label>
-        <input className="bg-white border-b-2 dark:bg-black text-inherit dark:border-gray-700 focus:border-black dark:focus:border-white focus:outline-none" id="fontSize" type="number" placeholder='12' min="12" max="90" value={options.fontSize} onChange={(e) => setOptions.fontSize(e.target.value)} />
+        <input className={inputCSS} id="fontSize" type="number" placeholder='24' min="24" max="90" value={options.fontSize} onChange={(e) => {
+            setOptions.fontSize(e.target.value)
+        }} />
 
         <label htmlFor="amount">Per page:</label>
-        <select className="bg-white text-inherit dark:bg-black" name="amount" id="amount" defaultValue="50" value={options.amount} onChange={(e) => {
+        <select className="bg-white text-inherit dark:bg-black" name="amount" id="amount" value={options.amount} onChange={(e) => {
             setOptions.amount(parseInt(e.target.value));
         }}>
             <option value="25">25</option>
@@ -44,5 +46,6 @@ export default function Filters({ setOptions, options }) {
         <button onClick={() => toggleTheme()} className="p-1 rounded-full hover:bg-neutral-100 dark:hover:bg-neutral-800">
             {theme == 'dark' ? <MoonSVG /> : <SunSVG />}
         </button>
+
     </div>)
 }
