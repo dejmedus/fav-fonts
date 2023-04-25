256f2852321f0ffe176e4268c14e94595fbb7501
dejmedus <email96123@gmail.com>
2023-04-24 23:39:54 -0600

    add faux link pages

---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+
+export default function About() {
+
+    return (
+        <main className="flex flex-col items-center justify-between min-h-screen p-2 sm:p-6 md:p-12 lg:p-24">
+            <h2>about</h2>
+        </main>
+    )
+}
