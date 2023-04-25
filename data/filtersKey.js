a321d1b303addc7860c6c4e72015619ce01fc0f3
dejmedus <email96123@gmail.com>
2023-04-25 12:20:55 -0600

    rename var handwriting > written

--- data/filtersKey.js
+++ data/filtersKey.js
@@ -4,7 +4,7 @@ export const filtersKey = {
     'cute': ['Balsamiq Sans', 'Gamja Flower', 'Bonbon', 'Capriola', 'Dongle', 'Slackey'],
     'fancy': ['Almendra', 'Almendra Display', 'Astloch', 'Bokor', 'IM Fell English SC', 'Grenze Gotisch', 'UnifrakturMaguntia', 'Pirata One'],
     'fun': ['Barrio', 'Averia Gruesa Libre', 'Rampart One', 'Nabla', 'BIZ UDMincho', 'Amarante', 'Berkshire Swash'],
-    'handwriting': ['Bad Script', 'Beth Ellen', 'Dawning of a New Day', 'La Belle Aurore'],
+    'written': ['Bad Script', 'Beth Ellen', 'Dawning of a New Day', 'La Belle Aurore'],
     'modern': ['DM Serif Text', 'Yatra One', 'Varela', 'Inter Tight', 'Krub', 'Allerta', 'Darker Grotesque', 'Krona One', 'Alexandria'],
     'space': ['Bungee Hairline', 'Bruno Ace', 'Audiowide', 'Bai Jamjuree', 'Share Tech Mono', 'Michroma', 'Oxanium', 'Goldman'],
 }
