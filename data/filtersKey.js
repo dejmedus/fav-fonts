8f8c13df36a665c1331d4d6fb31b48a1a3dedef5
dejmedus <email96123@gmail.com>
2023-04-25 14:42:05 -0600

    update filters key

--- data/filtersKey.js
+++ data/filtersKey.js
@@ -1,10 +1,11 @@
 export const filtersKey = {
     'bold': ['Bebas Neue', 'Londrina Solid', 'Koulen', 'Bowlby One SC', 'Candal', 'Knewave'],
-    'code': ['IBM Plex Mono', 'Space Mono', 'Anonymous Pro', 'JetBrains Mono', 'Overpass Mono', 'M PLUS 1'],
-    'cute': ['Balsamiq Sans', 'Gamja Flower', 'Bonbon', 'Capriola', 'Dongle', 'Slackey'],
+    'code': ['IBM Plex Mono', 'Space Mono', 'Anonymous Pro', 'JetBrains Mono', 'Overpass Mono', 'M PLUS 1', 'Source Code Pro', 'Epilogue'],
+    'cute': ['Balsamiq Sans', 'Gamja Flower', 'Bonbon', 'Capriola', 'Dongle', 'Slackey', 'Itim', 'Lemonada'],
     'fancy': ['Almendra', 'Almendra Display', 'Astloch', 'Bokor', 'IM Fell English SC', 'Grenze Gotisch', 'UnifrakturMaguntia', 'Pirata One'],
     'fun': ['Barrio', 'Averia Gruesa Libre', 'Rampart One', 'Nabla', 'BIZ UDMincho', 'Amarante', 'Berkshire Swash'],
-    'written': ['Bad Script', 'Beth Ellen', 'Dawning of a New Day', 'La Belle Aurore'],
-    'modern': ['DM Serif Text', 'Yatra One', 'Varela', 'Inter Tight', 'Krub', 'Allerta', 'Darker Grotesque', 'Krona One', 'Alexandria'],
-    'space': ['Bungee Hairline', 'Bruno Ace', 'Audiowide', 'Bai Jamjuree', 'Share Tech Mono', 'Michroma', 'Oxanium', 'Goldman'],
+    'written': ['Bad Script', 'Beth Ellen', 'Dawning of a New Day', 'La Belle Aurore', 'Caveat', 'Satisfy'],
+    'modern': ['DM Serif Text', 'Yatra One', 'Varela', 'Inter Tight', 'Krub', 'Allerta', 'Darker Grotesque', 'Krona One', 'Alexandria', 'Prompt', 'Material Symbols Outlined', 'Jura'],
+    'retro': ['Righteous', 'Monoton', 'Shrikhand'],
+    'space': ['Bungee Hairline', 'Bruno Ace', 'Audiowide', 'Bai Jamjuree', 'Share Tech Mono', 'Michroma', 'Oxanium', 'Goldman', 'Orbitron', 'Changa']
 }
