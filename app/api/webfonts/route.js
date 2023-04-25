ec9f4df57db159a90a8f4ab721dcec65fb2268ee
dejmedus <email96123@gmail.com>
2023-04-24 23:59:24 -0600

    sort by popularity

--- app/api/webfonts/route.js
+++ app/api/webfonts/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    const res = await fetch(`https://www.googleapis.com/webfonts/v1/webfonts?key=${process.env.DATA_API_KEY}`);
+    const res = await fetch(`https://www.googleapis.com/webfonts/v1/webfonts?sort=popularity&key=${process.env.DATA_API_KEY}`);
     const data = await res.json();
 
     return NextResponse.json({ data })
