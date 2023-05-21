86d6b28868dd2c16468efd39d32c398b703af97a
dejmedus <email96123@gmail.com>
2023-05-20 22:40:46 -0600

    update about page

--- app/about/page.js
+++ app/about/page.js
@@ -2,8 +2,17 @@
 export default function About() {
 
     return (
-        <main className="flex flex-col items-center justify-between min-h-screen p-2 sm:p-6 md:p-12 lg:p-24">
-            <h2>about</h2>
+        <main className="flex flex-col min-h-screen p-2 sm:p-6 md:p-18 lg:p-24">
+            <h2 className="self-center mb-8">About</h2>
+            <div className="w-1/2 self-center">
+                <p>Find Google Fonts by theme.</p>
+                <p>
+                    Created for the <a className="underline hover:text-gray-800" href="https://github.com/chingu-voyages/soloproject-tier3-favfonts">Chingu pre-work solo project</a>
+                </p>
+                <p>
+                    View the <a className="underline hover:text-gray-800" href="https://github.com/dejmedus/fav-fonts">source code</a>
+                </p>
+            </div>
         </main>
     )
 }
