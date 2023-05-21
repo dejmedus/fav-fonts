07f0e4499a3942994a10760a2f6e1b1c53014d1a
dejmedus <email96123@gmail.com>
2023-05-20 22:40:34 -0600

    remove faux pages

--- app/articles/page.js
+++
@@ -1,9 +0,0 @@
-
-export default function Article() {
-
-    return (
-        <main className="flex flex-col items-center justify-between min-h-screen p-2 sm:p-6 md:p-12 lg:p-24">
-            <h2>articles</h2>
-        </main>
-    )
-}
--- app/featured/page.js
+++
@@ -1,9 +0,0 @@
-
-export default function Featured() {
-
-  return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-2 sm:p-6 md:p-12 lg:p-24">
-      <h2>featured</h2>
-    </main>
-  )
-}
