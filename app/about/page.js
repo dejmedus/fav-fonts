0b3c011888ff86b4bb99c34fbd9a9a8f4931582d
dejmedus <email96123@gmail.com>
2023-07-13 12:50:30 -0600

    update about

--- app/about/page.js
+++ app/about/page.js
@@ -4,13 +4,14 @@ export default function About() {
     return (
         <main className="flex flex-col min-h-screen p-2 sm:p-6 md:p-18 lg:p-24">
             <h2 className="self-center mb-8">About</h2>
-            <div className="w-1/2 self-center">
+            <div className="self-center w-1/2">
                 <p>Find Google Fonts by theme.</p>
-                <p>
-                    Created for the <a className="underline hover:text-gray-800" href="https://github.com/chingu-voyages/soloproject-tier3-favfonts">Chingu pre-work solo project</a>
-                </p>
+                <p>Built with Nextjs, JavaScript, Google Fonts API, and TailwindCSS</p>
                 <p>
                     View the <a className="underline hover:text-gray-800" href="https://github.com/dejmedus/fav-fonts">source code</a>
+                </p>
+                <p>
+                    <a className="underline hover:text-gray-800" href="https://www.juliab.dev">Say hi</a>
                 </p>
             </div>
         </main>
